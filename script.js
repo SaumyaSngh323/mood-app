3a42ea1bb2c8b9ca6456d193ebe19576978f2872
git_pro <ay548026@gmail.com>
2026-03-26 19:13:18 +0530

    Refactor theme switcher code formatting

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const buttons = document.querySelectorAll('.theme-selector button');
 buttons.forEach(btn => {
   btn.addEventListener('click', () => {
-    document.body.classList.remove('theme-cute', 'theme-classy', 'theme-dark');
+    document.body.classList.remove('theme-cute','theme-classy','theme-dark');
     document.body.classList.add(`theme-${btn.dataset.theme}`);
   });
 });
