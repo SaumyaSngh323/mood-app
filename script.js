1d1d005d6c94c249d7cd4d8c04a9423415426d34
git_pro <ay548026@gmail.com>
2026-03-26 19:27:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// Theme Switcher
+// Select all theme buttons
 const buttons = document.querySelectorAll('.theme-selector button');
+
 buttons.forEach(btn => {
   btn.addEventListener('click', () => {
+    // Remove old themes
     document.body.classList.remove('theme-cute','theme-classy','theme-dark');
+    // Add new theme
     document.body.classList.add(`theme-${btn.dataset.theme}`);
   });
 });
